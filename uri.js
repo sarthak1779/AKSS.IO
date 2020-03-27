6db1d461be1dc17f46b69ee0c8db8eb27a83ea10
sarthak1779 <dsatmail2@gmail.com>
2020-03-27 11:10:14 +0530

    NEW PARTS IN RIDDLES
    BYSS

--- uri.js
+++ uri.js
@@ -5,9 +5,58 @@ function check() {
  var a = document.getElementById('input').value ;   		  
 if (a==="MATCH STICK") {
 
-            alert('write');
-            return true ;
+            alert('RIGHT');
+            window.location.href = "riddle p2.html" ;
     	}else{
-    	alert('wrong')
+    	alert('WRONG')
     }
     }
+
+    function checktwo() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="10") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p3.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
+    function checkthree() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="1") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p4.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
+    function checkfour() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="1") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p5.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
+    function checkfive() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="CLOCK") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p6.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
