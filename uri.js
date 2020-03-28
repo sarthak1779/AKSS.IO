6b161edc32854aea23a009346ba9db6d756dba38
sarthak1779 <dsatmail2@gmail.com>
2020-03-28 09:33:30 +0530

    new levels in riddles game
    by ss

--- uri.js
+++ uri.js
@@ -60,3 +60,87 @@ if (a==="CLOCK") {
     }
     }
 
+    function checksix() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="DAVID") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p7.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
+    function checkseven() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="FOOTSTEPS") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p8.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
+    function checkeight() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="YOUR NAME") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p9.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
+    function checknine() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="PIANO") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p10.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
+    function checkten() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="YESTERDAY,TODAY,TOMORROW") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p11.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
+    function checkeleven() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="PIANO") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p6.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
+    function checktwelve() {
+ 
+ var a = document.getElementById('input').value ;   		  
+if (a==="PIANO") {
+
+            alert('RIGHT');
+            window.location.href = "riddle p6.html" ;
+    	}else{
+    	alert('WRONG')
+    }
+    }
+
