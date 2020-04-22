615d8fce3180b1460de313478decdad241007b6f
sarthak1779 <dsatmail2@gmail.com>
2020-04-22 08:53:53 +0530

    saryhak

--- chat.js
+++ chat.js
@@ -9,8 +9,16 @@ if (a==="what is your name") {
          one.play();              
     	}else if (a==="who created you") {
          two.play();
+    	} else if (a==="open riddles game") {
+           window.location.href = "riddle p1.html"
+    	} else if (a==="open quiz") {
+    		window.location.href = "quiz part 1.html"
     	} else {
     		un.play();
+    	} {
+    		
+    	} {
+    		
 
     	} {
     	
---
+++ verify.js
@@ -0,0 +1,11 @@
+ function verification() {
+ 
+ var a = document.getElementById('password').value ;   		  
+if (a==="sarthak@1779") {
+
+           
+            window.location.href = "index1.html" ;
+    	}else{
+    	alert('invalid password')
+    }
+    }
