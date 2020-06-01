578684d63abe98faf182c34d74d613cc84197d28
sarthak1779 <dsatmail2@gmail.com>
2020-06-01 09:48:24 +0530

    style changes
    by ss

---
+++ cro.js
@@ -0,0 +1,9 @@
+function ban()
+ {
+ 	var a = parseInt(document.getElementById('ino1').value);
+ 	var b = parseInt(document.getElementById('ino2').value);
+ 	var result = a + b;
+ 	document.getElementById('kis').innerHTML=result;
+ 	
+	// body...
+}
---
+++ daddy.js
@@ -0,0 +1,12 @@
+function mont() {
+	var mont = document.getElementById('i1').value;
+	document.getElementById('m1').innerHTML = mont;
+}
+function school() {
+	var sch = document.getElementById('bb').value;
+	document.getElementById('bb1').innerHTML = sch;
+}
+function nyay() {
+	var ny = document.getElementById('cc').value;
+	document.getElementById('cc1').innerHTML = ny;
+}
