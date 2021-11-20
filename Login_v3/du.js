16b97ecc57dfca96700c82d04652c353362dda21
sarthak1779 <dsatmail2@gmail.com>
2021-11-20 19:24:24 +0530

    master
    master

---
+++ Login_v3/du.js
@@ -0,0 +1,14 @@
+
+
+
+
+function du(){
+	a = document.getElementById('dui').value ;
+	 if (a === "leaderss") {
+  	window.location.href = "riddle p1.html" ;
+  } else {
+  	alert('er') ;
+  }
+}
+
+ 
