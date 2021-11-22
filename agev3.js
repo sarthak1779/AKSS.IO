005f037c28d9b078b5f1d3c63b1e93d314677f77
sarthak1779 <dsatmail2@gmail.com>
2021-11-22 17:08:23 +0530

    updatedd
    updateddd

--- agev3.js
+++ agev3.js
@@ -7,7 +7,15 @@ var curYear = today.getFullYear();
 
 var ageCur = curYear - yy
 
-document.getElementById('rev').innerHTML =  ageCur + "years"
+document.getElementById('rev').innerHTML =  ageCur + "" + "Year(s)"
 
 
+}
+function Ageto(){
+  var dyear = document.getElementById('yy2').value;
+  var tyear = document.getElementById('yy3').value
+
+  var ageto = tyear - dyear
+
+  document.getElementById('rev2').innerHTML =  ageto + "Years(s)"
 }
